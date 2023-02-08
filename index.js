830f3ae01d8998525a8770c1d4568080cbe73fd5
311 <ismoilov92@gmail.com>
2023-02-09 00:33:23 +0300

    вывод результата

--- index.js
+++ index.js
@@ -11,9 +11,17 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname+"/views/index.html")
 })
 
+app.get('/how-it-work', (req, res)=>{
+    res.sendFile(__dirname+"/views/howItWork.html")
+})
+
+app.get('/author', (req, res)=>{
+    res.sendFile(__dirname+"/views/author.html")
+})
+
 app.post('/check-user', (req, res)=>{
     let username = req.body.username
-    res.send({ message: "OK", user: findUser(username) , otvet: "dasdsa"});
+    res.send({ message: findUser(username)});
 })
 
 
@@ -21,9 +29,9 @@ function findUser(username){
     let usersList = userList.getUsers().split(" ");
 
     if(usersList.find(el=>el===username))
-        return true;
+        return `Пользователь ${username} есть`;
     else
-        return false;
+        return `Пользователь ${username} нет`;
 }
 const PORT =3000
 
